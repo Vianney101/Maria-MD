dd2f9d82a77912758b52a4b9c724c04c9b9ad1d3
Vianney101 <165646086+Vianney101@users.noreply.github.com>
2024-04-01 10:59:50 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernumber = '25779845056'
-global.ownername = "‎Maestro Vianney"//owner name
+global.ownername = "‎Maestro"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
